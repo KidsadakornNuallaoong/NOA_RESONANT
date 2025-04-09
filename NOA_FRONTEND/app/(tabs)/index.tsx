becb084e1f28bb253ea5d2bfa3e358ff791ef7bf
KidsadakornNuallaoong <121489701+KidsadakornNuallaoong@users.noreply.github.com>
2025-04-09 16:10:36 +0700

    test

--- NOA_FRONTEND/app/(tabs)/index.tsx
+++ NOA_FRONTEND/app/(tabs)/index.tsx
@@ -16,8 +16,6 @@ export default function TabOneScreen() {
         laborum aut illo iste, eveniet sapiente! Aliquid, laudantium doloribus?
       </Text>
       <Text>abc</Text>
-      {/* Hello */}
-      <Text>abc</Text>
     </View>
   );
 }
